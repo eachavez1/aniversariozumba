bdec5d8cfb165b9eed134f053dcd05b4b4396400
eachavez1 <slipkanot2010@hotmail.com>
2024-02-05 19:36:15 -0500

    updated

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 
+//ANIMACIÓN DEL CONTENIDO DEL MAIN //
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Animar el contenido del main
+    var mainContent = document.querySelector('main');
+    mainContent.classList.add('visible');
+  });
